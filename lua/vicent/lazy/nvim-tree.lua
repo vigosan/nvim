5440bff22cbe76c2b219336f18ad16f6a537dfe4
Vicent Gozalbes <vigosan@gmail.com>
2024-04-23 10:43:27 +0200

    configure nvim tree

--- lua/vicent/lazy/nvim-tree.lua
+++ lua/vicent/lazy/nvim-tree.lua
@@ -1,30 +1,49 @@
 -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/lua/nvim-tree.lua#L344
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	lazy = false,
-	config = function()
-		vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  config = function()
+    vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
-		require("nvim-web-devicons").setup()
-		require("nvim-tree").setup({
-			disable_netrw = true,
-			view = {
-				side = "right",
-			},
-			actions = {
-				open_file = {
-					quit_on_open = true,
-				},
-			},
-			update_focused_file = {
-				enable = true,
-			},
-			filters = {
-				dotfiles = true,
-			},
-		})
-	end,
+    require("nvim-tree").setup({
+      disable_netrw = true,
+      view = {
+        side = "right",
+        width = 36,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+      update_focused_file = {
+        enable = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          glyphs = {
+            default = "󰦨",
+            symlink = "󰦨",
+            bookmark = "󰦨",
+            git = {
+              unstaged = "",
+              staged = "",
+              unmerged = "",
+              renamed = "",
+              deleted = "",
+              untracked = "",
+              ignored = "",
+            },
+          },
+        },
+      },
+    })
+  end,
 }
