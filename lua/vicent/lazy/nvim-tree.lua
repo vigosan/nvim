57bbe160aec71dab700b1ec6e76169715de91dca
Vicent Gozalbes <vigosan@gmail.com>
2024-04-23 10:21:47 +0200

    fix typo

--- lua/vicent/lazy/nvim-tree.lua
+++ lua/vicent/lazy/nvim-tree.lua
@@ -1,30 +1,30 @@
 -- https://github.com/nvim-tree/nvim-tree.lua/blob/master/lua/nvim-tree.lua#L344
 return {
-  'nvim-tree/nvim-tree.lua',
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  lazy = false,
-  config = function()
-    vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	config = function()
+		vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
-    require'nvim-web-devicons'.setup()
-    require("nvim-tree").setup({
-      disable_netrw = true,
-      view = {
-        side = "right"
-      },
-      actions = {
-        open_file = {
-          quit_on_open = true,
-        }
-      },
-      update_focused_file = {
-        enable = true,
-      },
-      filters = {
-        dotfiles = true
-      }
-    })
-  end
+		require("nvim-web-devicons").setup()
+		require("nvim-tree").setup({
+			disable_netrw = true,
+			view = {
+				side = "right",
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+			update_focused_file = {
+				enable = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		})
+	end,
 }
