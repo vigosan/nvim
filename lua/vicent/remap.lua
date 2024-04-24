693c623e20866df8b32f9e70f0fc02a9ee16f09f
Vicent Gozalbes <vigosan@gmail.com>
2024-04-24 13:36:01 +0200

    add more maps

--- lua/vicent/remap.lua
+++ lua/vicent/remap.lua
@@ -1,5 +1,14 @@
 vim.g.mapleader = ","
+
 vim.keymap.set("n", "<leader><leader>", "<c-^>")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
