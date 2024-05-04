bf0a0f5221ebaed8cd421d67c6fe24dab0fcda1a
Vicent Gozalbes <vigosan@gmail.com>
2024-05-04 18:47:26 +0200

    create folder if not exists

--- lua/vicent/init.lua
+++ lua/vicent/init.lua
@@ -8,6 +8,7 @@ autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   callback = function(args)
     save_cursor = vim.fn.getpos(".")
+    vim.cmd('silent! !mkdir -p %:p:h')
     vim.cmd([[%s/\s\+$//e]])
     vim.fn.setpos(".", save_cursor)
   end,
