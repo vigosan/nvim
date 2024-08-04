49a012a5196c3fd6deded5f5b18e25823d83e71c
Vicent Gozalbes <vigosan@gmail.com>
2024-08-04 18:55:29 +0200

    update version and lint

--- lua/vicent/lazy/lsp.lua
+++ lua/vicent/lazy/lsp.lua
@@ -1,65 +1,70 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-				-- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-				ensure_installed = {
-					"cssls",
-					"eslint",
-					"html",
-					"lua_ls",
-					"solargraph",
-					"standardrb",
-					"tailwindcss",
-					"tsserver",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+                ensure_installed = {
+                    "cssls",
+                    "emmet_ls",
+                    "eslint",
+                    "html",
+                    "lua_ls",
+                    "solargraph",
+                    "standardrb",
+                    "tailwindcss",
+                    "tsserver",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.eslint.setup({ capabilities = capabilities })
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.solargraph.setup({ capabilities = capabilities })
-			lspconfig.standardrb.setup({ capabilities = capabilities })
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				settings = {
-					tailwindCSS = {
-						experimental = {
-							classRegex = {
-								"*(?:(clsx|cn))\\(([^)]*)\\)",
-								"(?:'|\"|`)([^\"'`]*)(?:'|\"|`)",
-								"*(?:Class|Classes)?=\\s*(?:\"|'|{`)([^(?:\"|'|`})]*)",
-							},
-						},
-						validate = true,
-					},
-				},
-			})
-			lspconfig.tsserver.setup({ capabilities = capabilities })
+            lspconfig.cssls.setup({ capabilities = capabilities })
+            lspconfig.emmet_ls.setup({
+                capabilities = capabilities,
+                filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "scss", "typescriptreact" },
+            })
+            lspconfig.eslint.setup({ capabilities = capabilities })
+            lspconfig.html.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.solargraph.setup({ capabilities = capabilities })
+            lspconfig.standardrb.setup({ capabilities = capabilities })
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                settings = {
+                    tailwindCSS = {
+                        classAttributes = { 'class', 'className', '.*Classes', 'tw' },
+                        validate = true,
+                        experimental = {
+                            classRegex = {
+                                ":class\\s+\"([^\"]*)\"",
+                                ":[\\w-.#>]+\\.([\\w-]*)"
+                            },
+                        }
+                    }
+                }
+            })
+            lspconfig.tsserver.setup({ capabilities = capabilities })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
--- lua/vicent/lazy/none-ls.lua
+++ lua/vicent/lazy/none-ls.lua
@@ -1,17 +1,36 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
-      },
-    })
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        -- Adding this as a dependency because some of the default lsps were removed
+        -- See https://github.com/nvimtools/none-ls.nvim/discussions/81 for more information
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- formatting
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier.with {
+                    filetypes = {
+                        "html",
+                        "css",
+                        "scss",
+                        "javascript",
+                        "typescript",
+                        "json",
+                        "yaml",
+                        "markdown",
+                    },
+                },
+                require("none-ls.formatting.standardrb"),
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
-  end,
+                -- diagnostics
+                require("none-ls.diagnostics.eslint"),
+                -- require("none-ls.diagnostics.standardrb")
+            },
+        })
+
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
+    end,
 }
