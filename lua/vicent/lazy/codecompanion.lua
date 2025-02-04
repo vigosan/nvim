31002c044de5b450c5d1ff640b99d7e36f873843
Vicent Gozalbes <vigosan@gmail.com>
2025-02-04 10:35:06 +0100

    configure codecompanion

---
+++ lua/vicent/lazy/codecompanion.lua
@@ -0,0 +1,39 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "github/copilot.vim"
+  },
+  config = function()
+    require("codecompanion").setup({
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {})
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
+        },
+        agent = {
+          adapter = "copilot",
+        },
+      },
+      schema = {
+        model = {
+          default = "claude-3.5-sonnet",
+        },
+      }
+    })
+
+    vim.keymap.set("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", {})
+    vim.keymap.set("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", {})
+    vim.keymap.set("n", "<leader>c", "<cmd>CodeCompanionChat Toggle<cr>i", {})
+    vim.keymap.set("v", "<leader>c", "<cmd>CodeCompanionChat Toggle<cr>i", {})
+    vim.keymap.set("v", "gc", "<cmd>CodeCompanionChat Add<cr>", {})
+
+    vim.cmd([[cab cc CodeCompanion]])
+  end,
+}
